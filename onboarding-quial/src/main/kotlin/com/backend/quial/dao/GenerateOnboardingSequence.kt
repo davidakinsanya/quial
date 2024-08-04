f7bf6cc7d27588d606291dbe11c9c0c57c4092d4
davidakinsanya <davidakinsanya00@gmail.com>
2024-08-04 20:46:51 +0100

    getting rid of unused imports.

--- onboarding-quial/src/main/kotlin/com/backend/quial/dao/GenerateOnboardingSequence.kt
+++ onboarding-quial/src/main/kotlin/com/backend/quial/dao/GenerateOnboardingSequence.kt
@@ -1,9 +1,6 @@
 package com.backend.quial.dao
 
-import com.backend.quial.dto.OnboardingEnums
 import com.backend.quial.dto.OnboardingSequence
-import com.backend.quial.dto.Question
-import com.backend.quial.dto.Statement
 import com.beust.klaxon.Klaxon
 import java.io.File
 
