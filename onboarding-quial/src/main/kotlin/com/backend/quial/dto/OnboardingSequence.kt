6ed17eb25677752edd4bd5eb47a8d194289ac0c3
davidakinsanya <davidakinsanya00@gmail.com>
2024-08-05 13:25:50 +0100

    Implementing a mapping technique to differentiate between a question and statement object.

--- onboarding-quial/src/main/kotlin/com/backend/quial/dto/OnboardingSequence.kt
+++ onboarding-quial/src/main/kotlin/com/backend/quial/dto/OnboardingSequence.kt
@@ -8,4 +8,40 @@ import kotlinx.serialization.Serializable
  * @author David.
  */
 @Serializable
-sealed interface OnboardingSequence
+sealed interface OnboardingSequence {
+
+    /**
+     * This method returns a string object for the type of element in the onboarding sequence.
+     *
+     * @return a String representing a sequence element.
+     */
+    fun getSequenceType(): String
+
+    /**
+     * This method returns a string object for the header of a Statement object of the onboarding sequence.
+     *
+     * @return a String representing a header.
+     */
+    fun getHeader(): String?
+
+    /**
+     * This method returns a string object for the question of a Question object of the onboarding sequence.
+     *
+     * @return a String representing a question.
+     */
+    fun getQuestion(): String?
+
+    /**
+     * This method returns a string object for the text of a Statement object of the onboarding sequence.
+     *
+     * @return a String representing text.
+     */
+    fun getText(): String?
+
+    /**
+     * This method returns a list object for the answer options of a Question object of the onboarding sequence.
+     *
+     * @return a list object representing a list of answer options.
+     */
+    fun getOptions(): List<String>?
+}
--- onboarding-quial/src/main/kotlin/com/backend/quial/dto/Question.kt
+++ onboarding-quial/src/main/kotlin/com/backend/quial/dto/Question.kt
@@ -5,4 +5,50 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Question(val enum: String = OnboardingEnums.QUESTION.name,
                     val question: String,
-                    val options: List<String>): OnboardingSequence
+                    val options: List<String>): OnboardingSequence {
+
+    /**
+     * This method returns a string object for the type of element in the onboarding sequence.
+     *
+     * @return a String representing a sequence element.
+     */
+    override fun getSequenceType(): String {
+        return this.enum
+    }
+
+    /**
+     * This method returns a string object for the header of a Statement object of the onboarding sequence.
+     *
+     * @return a String representing a header.
+     */
+    override fun getHeader(): String? {
+        return null
+    }
+
+    /**
+     * This method returns a string object for the question of a Question object of the onboarding sequence.
+     *
+     * @return a String representing a question.
+     */
+    override fun getQuestion(): String {
+       return this.question
+    }
+
+    /**
+     * This method returns a string object for the text of a Statement object of the onboarding sequence.
+     *
+     * @return a String representing text.
+     */
+    override fun getText(): String? {
+       return null
+    }
+
+    /**
+     * This method returns a list object for the answer options of a Question object of the onboarding sequence.
+     *
+     * @return a list object representing a list of answer options.
+     */
+    override fun getOptions(): List<String> {
+        return this.options
+    }
+}
