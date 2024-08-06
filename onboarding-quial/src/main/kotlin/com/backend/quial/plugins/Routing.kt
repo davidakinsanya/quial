583f54e6f1f95fba218de2714ef3edf17b421e88
davidakinsanya <davidakinsanya00@gmail.com>
2024-08-06 04:44:03 +0100

    Converting get request into a post request to accept json.

--- onboarding-quial/src/main/kotlin/com/backend/quial/plugins/Routing.kt
+++ onboarding-quial/src/main/kotlin/com/backend/quial/plugins/Routing.kt
@@ -1,18 +1,21 @@
 package com.backend.quial.plugins
 
 import com.backend.quial.dao.GenerateOnboardingSequence
+import com.backend.quial.dto.OnboardingSequence
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respond(GenerateOnboardingSequence().loadSequence()!!)
+            call.respond(GenerateOnboardingSequence().loadSequence())
         }
 
-        get("/generate/{json}") {
-            call.respond(GenerateOnboardingSequence().generateNewSequence(call.parameters["json"]!!))
+        post("/generate") {
+            val json = call.receive(OnboardingSequence::class)
+            call.respond(GenerateOnboardingSequence().generateNewSequence(json.toString()))
         }
     }
 }
