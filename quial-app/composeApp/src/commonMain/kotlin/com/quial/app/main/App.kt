7c8abdcd9ba112ebaca36b8a9f3945aa7a181957
davidakinsanya <davidakinsanya00@gmail.com>
2024-09-26 02:18:34 +0100

    don't think I had di issues, however I think i should launch the app from the navigation with screen model objects.

--- quial-app/composeApp/src/commonMain/kotlin/com/quial/app/main/App.kt
+++ quial-app/composeApp/src/commonMain/kotlin/com/quial/app/main/App.kt
@@ -14,11 +14,13 @@ import com.quial.app.http.requests.OnboardingClient
 import com.quial.app.repository.OnboardingRepository
 import com.quial.app.screen.onboarding.OnboardingScreen
 import com.quial.app.screen.onboarding.OnboardingUiStateHolder
+import com.quial.app.utils.getUiStateHolder
 import io.ktor.client.HttpClient
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinContext
 import org.koin.compose.koinInject
+import com.quial.app.utils.getUiStateHolder
 
 import quial_app.composeapp.generated.resources.Res
 import quial_app.composeapp.generated.resources.compose_multiplatform
@@ -28,15 +30,23 @@ import quial_app.composeapp.generated.resources.compose_multiplatform
 fun App() {
 
     MaterialTheme {
-        KoinContext {
-            val onboardingUiStateHolder = koinInject<OnboardingUiStateHolder>()
-
-            OnboardingScreen(
-                modifier = Modifier,
-                uiStateHolder = onboardingUiStateHolder,
-                onNavigateMain = {}
-            )
-
+        var showContent by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = { showContent = !showContent }) {
+                Text("Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
+                Column(
+                    Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting")
+                }
+            }
         }
+
     }
+
 }
