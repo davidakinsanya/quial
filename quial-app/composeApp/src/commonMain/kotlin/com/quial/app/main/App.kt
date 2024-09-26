9caf460f99f23719a2d13154b175a68ff7c767a4
davidakinsanya <davidakinsanya00@gmail.com>
2024-09-26 02:20:38 +0100

    cleanup

--- quial-app/composeApp/src/commonMain/kotlin/com/quial/app/main/App.kt
+++ quial-app/composeApp/src/commonMain/kotlin/com/quial/app/main/App.kt
@@ -10,18 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.quial.app.http.requests.OnboardingClient
-import com.quial.app.repository.OnboardingRepository
-import com.quial.app.screen.onboarding.OnboardingScreen
-import com.quial.app.screen.onboarding.OnboardingUiStateHolder
-import com.quial.app.utils.getUiStateHolder
-import io.ktor.client.HttpClient
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.compose.KoinContext
-import org.koin.compose.koinInject
-import com.quial.app.utils.getUiStateHolder
-
 import quial_app.composeapp.generated.resources.Res
 import quial_app.composeapp.generated.resources.compose_multiplatform
 
@@ -46,7 +36,5 @@ fun App() {
                 }
             }
         }
-
     }
-
 }
