b7320a7e2fa8272e6bf0236f8fbfcd227694f2ed
davidakinsanya <58745820+davidakinsanya@users.noreply.github.com>
2025-01-20 23:33:11 +0000

    Adding new files dor my new app icon (v32)

--- quial-app/composeApp/build.gradle.kts
+++ quial-app/composeApp/build.gradle.kts
@@ -206,7 +206,7 @@ kotlin {
             applicationId = "com.quial.app"
             minSdk = libs.versions.android.minSdk.get().toInt()
             targetSdk = libs.versions.android.targetSdk.get().toInt()
-            versionCode = 31
+            versionCode = 32
             versionName = "1.0"
         }
         packaging {
