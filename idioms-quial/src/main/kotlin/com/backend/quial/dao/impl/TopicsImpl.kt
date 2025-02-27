8892650c7fd1baf8e802f0e7069edbc10e86d1cd
davidakinsanya <58745820+davidakinsanya@users.noreply.github.com>
2025-02-27 04:53:56 +0000

    Altering api!

--- idioms-quial/src/main/kotlin/com/backend/quial/dao/impl/TopicsImpl.kt
+++ idioms-quial/src/main/kotlin/com/backend/quial/dao/impl/TopicsImpl.kt
@@ -12,8 +12,6 @@ class TopicsImpl: Topics {
         val fileCheck = File(Paths.get(System.getProperty("user.dir"),
             "../usr/src/app/topics.txt").toAbsolutePath().toString()).exists()
 
-        println(Paths.get(System.getProperty("user.dir")).toString())
-
         val file = if (fileCheck) {
             File(Paths.get(System.getProperty("user.dir"),
                 "../usr/src/app/topics.txt")
@@ -43,24 +41,28 @@ class TopicsImpl: Topics {
 
     override fun getIdiomsByTopic(topic: String): List<Idiom> {
         val listOfIdioms = mutableListOf<Idiom>()
-        val file = File(Paths.get(System.getProperty("user.dir"),
-            "../usr/src/app/${topic}-quial.csv").toAbsolutePath().toString())
+        try {
+            val file = File(Paths.get(System.getProperty("user.dir"),
+                "../usr/src/app/${topic}-quial.csv").toAbsolutePath().toString())
 
-        val regex = """\[(.*?)]""".toRegex()
+            val regex = """\[(.*?)]""".toRegex()
 
-        val csvReader = csvReader {
-            skipEmptyLine = true
-        }
-        val list = csvReader.readAll(file).toList()
+            val csvReader = csvReader {
+                skipEmptyLine = true
+            }
+            val list = csvReader.readAll(file).toList()
 
-        for (i in 1..<list.size) {
-            listOfIdioms.add(
-                Idiom(
-                    regex.findAll(list[i][0]).map{it.groupValues[1]}.toList(),
-                    regex.findAll(list[i][1]).map{it.groupValues[1]}.toList(),
-                    regex.findAll(list[i][2]).map{it.groupValues[1]}.toList(),
+            for (i in 1..<list.size) {
+                listOfIdioms.add(
+                    Idiom(
+                        regex.findAll(list[i][0]).map{it.groupValues[1]}.toList(),
+                        regex.findAll(list[i][1]).map{it.groupValues[1]}.toList(),
+                        regex.findAll(list[i][2]).map{it.groupValues[1]}.toList(),
+                    )
                 )
-            )
+            }
+        } catch (e: Exception) {
+          println(e.message)
         }
 
         return listOfIdioms
