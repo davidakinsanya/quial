156fab8704b83f753cce762882f21b1620ff4cb7
davidakinsanya <58745820+davidakinsanya@users.noreply.github.com>
2025-03-17 01:33:23 +0000

    .

--- idioms-quial/src/main/kotlin/com/backend/quial/python/topic-quial.py
+++ idioms-quial/src/main/kotlin/com/backend/quial/python/topic-quial.py
@@ -34,22 +34,6 @@ def main_driver():
                      driver_executable_path = cd.install())
 
 
-
-
-def alt_meanings(idiom, idiom_list):
-    idiom_breakdown = idiom.split()
-    alt_list = []
-
-    for i in range (0, len(idiom_list)):
-        main_string = idiom_list[i]
-        for word in idiom_breakdown:
-            if word in main_string:
-                alt_list.append(main_string.replace('.', ' '))
-            break
-                
-    return alt_list[0].split('\n')
-    
-
 def get_additional_meaning(idiom, link):
     driver.get(link)
 
@@ -59,25 +43,10 @@ def get_additional_meaning(idiom, link):
     
     ul_elems_comp = [elem.text for elem in ul_elems]
     ol_elems_comp = [elem.text for elem in ol_elems]
-
-    meanings_list = ul_elems_comp[1].split('\n')
-
-    final_meanings = []
-    final_additional_meanings = []
-    arr_set = set(idiom.split()).intersection(meanings_list[0].split())
-
-    if (len(arr_set) == 0):
-        final_meanings = ul_elems_comp[1].split('\n')
-    else:
-        final_meanings = ul_elems_comp[2].split('\n')
-
-    if ('(64)' in meanings_list[0]):
-        final_additional_meanings = alt_meanings(idiom, ul_elems_comp)
-    else:
-        final_additional_meanings = ol_elems_comp[1].split('\n')
-
-
     
+    return [ul_elems_comp[1].split('\n'), ol_elems_comp[1].split('\n')]
+    
+
 def get_meaning(idiom):
     idiom_arr = idiom.split('\n')
     idiom_arr[1] = re.sub(r"\(.*?\)","()" ,idiom_arr[1]) # removes everything inside ()
@@ -86,7 +55,6 @@ def get_meaning(idiom):
         idiom_arr[2] = idiom_arr[2].replace(" Read more ➺", "")
         
     return idiom_arr
-
 
 def scrape_topic():
     driver.get(url + "topics/")
@@ -103,13 +71,11 @@ def scrape_topic():
     return topics_list
 
 
-
 def get_links(idioms):
     arr = []
     for i in range(0, len(idioms)):
         arr.append(idioms[i].find_elements(By.CSS_SELECTOR, "a")[0].get_attribute('href'))
     return arr
-
 
 def get_data(topic, idioms, links, alt):
     idioms_list = [text.text for text in idioms]
@@ -147,6 +113,7 @@ def main_scrape(topic, url, page):
     if page == 1:
         idiom_dict[topic] = [[], [], []]
         
+    idiom_for_letter = []
     driver.get(url + topic + "/page/" + str(page))
 
     page_info = driver.find_elements(By.CSS_SELECTOR, 'p.pno')
