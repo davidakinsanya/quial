ccd8dbf15a7a293232f00e3ea802af2f2e2181b7
davidakinsanya <58745820+davidakinsanya@users.noreply.github.com>
2025-03-18 11:00:43 +0000

    Adding new buttons to the bottom sheet!

--- quial-app/composeApp/src/commonMain/kotlin/com/quial/app/images/Images.kt
+++ quial-app/composeApp/src/commonMain/kotlin/com/quial/app/images/Images.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.InputModeManager
 import androidx.compose.ui.layout.ContentScale
 import org.jetbrains.compose.resources.painterResource
 import quial_app.composeapp.generated.resources.Res
